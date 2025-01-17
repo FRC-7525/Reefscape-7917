750128d3d653e8ec605d470f224c091d39c6996f
thomasmanaye <meigsthomasm@gmail.com>
2025-01-17 17:48:46 -0600

    Built Climber

---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.Climber;
+
+public final class ClimberConstants {
+    public static final double P = 0.1;
+    public static final double I = 0.1;
+    public static final double D = 0.1;
+
+    public static final int DEVICE_ID = 16;
+
+    // In rotations
+    public static final double IDLE_POS = 2;
+    public static final double OFF_POS = -1;
+
+    public static double GEAR_RATIO = 0;
+
+}
---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberStates.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.Climber;
+
+import static frc.robot.subsystems.Climber.ClimberConstants.IDLE_POS;
+import static frc.robot.subsystems.Climber.ClimberConstants.OFF_POS;
+
+import org.team7525.subsystem.SubsystemStates;
+
+public enum ClimberStates implements SubsystemStates{
+    IDLE("IDLE", IDLE_POS),
+    OFF("OFF", OFF_POS);
+    
+    private String stateString;
+    private double position;
+
+    ClimberStates(String stateString, double position) {
+        this.stateString = stateString;
+        this.position = position;
+    }
+
+    @Override
+    public String getStateString() {
+        return stateString;
+    }
+
+    public double getPosition() {
+        return position;
+    }
+    
+}
