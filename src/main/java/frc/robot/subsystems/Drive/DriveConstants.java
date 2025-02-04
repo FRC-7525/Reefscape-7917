47a34ff4c287c602b11f326a2ea824956368d9d5
Leif031809 <prisimrobotics@gmail.com>
2025-02-03 18:07:17 -0600

    actual IDs

--- src/main/java/frc/robot/subsystems/Drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/Drive/DriveConstants.java
@@ -6,6 +6,6 @@ import edu.wpi.first.units.measure.LinearVelocity;
 
 public final class DriveConstants {
 
-	public static final LinearVelocity MAX_SPEED = MetersPerSecond.of(16);
+	public static final LinearVelocity MAX_SPEED = MetersPerSecond.of(5);
 	public static final LinearVelocity SLOW_SPEED = MetersPerSecond.of(MAX_SPEED.magnitude() * 0.2);
 }
