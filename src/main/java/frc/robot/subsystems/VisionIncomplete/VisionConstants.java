5f6c085034c87f58f57f77881cc634c1960908a9
Aitan Martinez <prisimrobotics@gmail.com>
2025-02-16 18:41:25 -0600

    Add Vision subsystem with state management and IO interfaces (doesn't work yet)

---
+++ src/main/java/frc/robot/subsystems/VisionIncomplete/VisionConstants.java
@@ -0,0 +1,69 @@
+package frc.robot.subsystems.VisionIncomplete;
+
+import java.io.IOException;
+
+import org.team7525.misc.VisionUtil.CameraResolution;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+
+
+//TODO: replace values with actual values!
+public class VisionConstants {
+    public static final Translation3d ROBOT_TO_FRONT_CAMERA_TRANSLATION = new Translation3d(
+        Units.inchesToMeters(11.5), Units.inchesToMeters(-11.5), Units.inchesToMeters(9)
+    );
+    public static final Translation3d ROBOT_TO_BACK_CAMERA_TRANSLATION = new Translation3d(
+        Units.inchesToMeters(-11.5), Units.inchesToMeters(11.5), Units.inchesToMeters(9)
+    );
+    public static final Rotation3d ROBOT_TO_FRONT_CAMERA_ROTATION = new Rotation3d(
+        0,
+        Math.toRadians(0),
+        Math.toRadians(0)
+    );
+    public static final Rotation3d ROBOT_TO_BACK_CAMERA_ROTATION = new Rotation3d(
+        0,
+        Math.toRadians(0),
+        Math.toRadians(0)
+    );
+    
+    
+    
+    public static final Transform3d ROBOT_TO_FRONT_CAMERA = new Transform3d(
+        ROBOT_TO_FRONT_CAMERA_TRANSLATION, ROBOT_TO_FRONT_CAMERA_ROTATION
+    );
+    public static final Transform3d ROBOT_TO_BACK_CAMERA = new Transform3d(
+        ROBOT_TO_BACK_CAMERA_TRANSLATION, ROBOT_TO_BACK_CAMERA_ROTATION
+    );
+
+    public static final double CAMERA_DEBOUNCE_TIME = 0.5;
+
+    public static final CameraResolution FRONT_RESOLUTION = CameraResolution.HIGH_RES;
+    public static final CameraResolution BACK_RESOLUTION = CameraResolution.HIGH_RES;
+
+
+    public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT;
+
+    static {
+        AprilTagFieldLayout layout = null;
+        try {
+            layout = new AprilTagFieldLayout("src\\main\\java\\frc\\robot\\subsystems\\Vision\\2025-reefscape.json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        APRIL_TAG_FIELD_LAYOUT = layout;
+    }
+
+    public static final int CAMERA_WIDTH = 1200;
+    public static final int CAMERA_HEIGHT = 800;
+    public static final Rotation2d CAMERA_ROTATION = Rotation2d.fromDegrees(0);
+    public static final double CALIB_ERROR_AVG = 0.25;
+    public static final double CALIB_ERROR_STD_DEV = 0.08;
+    public static final int CAMERA_FPS = 40;
+    public static final int AVG_LATENCY_MS = 40;
+    public static final int LATENCY_STD_DEV_MS = 10;
+}
---
+++ src/main/java/frc/robot/subsystems/VisionIncomplete/VisionStates.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.VisionIncomplete;
+
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.team7525.subsystem.SubsystemStates;
+
+public enum VisionStates implements SubsystemStates {
+    ON("VISION ON", true, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR),
+    LOWEST_AMBIGUITY("Lowest Ambiguity", true, PoseStrategy.LOWEST_AMBIGUITY),
+    OFF("VISION OFF", false, PoseStrategy.CLOSEST_TO_LAST_POSE);
+
+    private boolean visionEnabled;
+    private PoseStrategy strategy;
+    private String stateString;
+
+    VisionStates(String stateString, boolean visionEnabled, PoseStrategy strategy) {
+        this.visionEnabled = visionEnabled;
+        this.strategy = strategy;
+        this.stateString = stateString;
+    }
+
+    public boolean getVisionEnabled() {
+        return visionEnabled;
+    }
+
+    public PoseStrategy getStrategy() {
+        return strategy;
+    }
+
+    @Override
+    public String getStateString() {
+        return stateString;
+    }
+}
